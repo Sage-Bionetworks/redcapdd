525f24fbe7809d3a9dfdde9d7f15f549c43d1299
Kara Woo <woo.kara@gmail.com>
2018-03-23 14:30:37 -0700

    No need to have an author name here, that's silly

--- R/create_template.R
+++ R/create_template.R
@@ -1,10 +1,8 @@
 #' Create empty data dictionary template
 #'
-#'
 #' @return An empty (0-row) data frame with columns corresponding to the column
 #'   names required in a REDCap data dictionary.
 #' @export
-#' @author Kara Woo
 create_dd_template <- function() {
   cols <- dd_cols
   cols <- enquo(cols)
